7101e35569049e12c9e3d3a6cdb3a77a8c8c973d
MarcelAlfonsoGarcia <marcel.alfonso99@gmail.com>
2022-03-02 20:23:28 -0600

    Adding error throwing

--- src/main/java/panel/ImagePanel.java
+++ src/main/java/panel/ImagePanel.java
@@ -1,7 +1,10 @@
 package panel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,18 +32,42 @@ public class ImagePanel extends Thread {
     }
 
     public static void nextImage() {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            return;
+        }
+
         if (images.size() == currentImage + 1) return;
         currentImage++;
         changeImage();
     }
 
     public static void previousImage() {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            return;
+        }
+
         if (currentImage == 0) return;
         currentImage--;
         changeImage();
     }
 
-    public static void startAnimation() {
+    public static void lastImage() {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            return;
+        }
+
+        currentImage = images.size() - 1;
+        changeImage();
+    }
+
+    public static void startAnimation() throws Exception {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            throw new IllegalArgumentException("No image selected");
+        }
         animated = true;
     }
 
@@ -63,7 +90,32 @@ public class ImagePanel extends Thread {
     }
 
     public static void changeImage() {
-    	var bi = images.get(currentImage);
+        var bi = images.get(currentImage);
+        ImageIcon icon = new ImageIcon(bi);
+        imageDisplay.setIcon(icon);
+
+        MainPanel.changeFrameSize(bi.getWidth() + 50, bi.getHeight() + 140);
+    }
+
+    public static void saveImage() throws IOException {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            return;
+        }
+        BufferedImage bi = images.get(currentImage);
+        File carvedImage = new File("./carvedImage.jpg");
+        ImageIO.write(bi, "JPEG", carvedImage);
+    }
+
+    public static void viewEnergy() {
+        if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "You have not submitted an image");
+            return;
+        }
+
+        var bi = images.get(currentImage);
+        // we will need a function that takes in a BufferedImage and calculates the energy into a BufferedImage arr
+        BufferedImage energyBI = null;
         ImageIcon icon = new ImageIcon(bi);
         imageDisplay.setIcon(icon);
 
